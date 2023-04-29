3e5fc4073eb717315b15f60360b102605d55e952
Goiko <juanmiguel.goicoechea@biko2.com>
2023-04-29 08:33:12 +0200

    servidor montado sin chalk

--- index.ts
+++ index.ts
@@ -1,2 +1,16 @@
-const uno :string = "goiko"
-console.log (uno)
+import { IncomingMessage, ServerResponse, createServer } from "http";
+
+const hostname = "localhost";
+const port = 3000;
+
+const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  const uno: string = "goiko";
+  res.end(uno);
+  console.log("Request!!");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
